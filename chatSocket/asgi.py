353f1a7af0527e19b90a658e82e7eb72838aa6b1
SalarKesha <keshavarzisalar@gmail.com>
2024-02-10 23:30:26 +0330

    initial

---
+++ chatSocket/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for chatSocket project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatSocket.settings')
+
+application = get_asgi_application()
